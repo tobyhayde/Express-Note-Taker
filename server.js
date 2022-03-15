4b2de856e49f7c9efe690f475f123afdbcdceb74
thayde <toby.hayde@icloud.com>
2022-03-14 19:52:24 -0500

    path's updated

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('/routes/apiroutes');
-const htmlRoutes = require('/routes/htmlRoutes');
+const apiRoutes = require('./routes/apiroutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 // Parse URL encoded & JSON
 app.use(express.urlencoded({ extended: true }));
